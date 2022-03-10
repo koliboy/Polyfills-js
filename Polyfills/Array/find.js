6062e1d59b08bd113f196cd6181e8674cf3ca7a3
im__koli <51367985+koliboy@users.noreply.github.com>
2022-03-11 01:35:27 +0530

    ok

--- Polyfills/Array/find.js
+++ Polyfills/Array/find.js
@@ -1,1 +1,12 @@
-
+ Array.prototype.find = function (fun,arg){
+     if(!(typeof fun == "function" || fun instanceof Function)){
+            throw TypeError;    
+          }
+      
+       for(let i =0; i < this.length; i++){
+        if(fun.call(arg,this[i],i,this)){
+           return this[i]
+        } 
+       }
+        
+}
