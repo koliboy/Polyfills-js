b3edd88ea434a4f0f47d3a9c2414a43a008582d2
im__koli <51367985+koliboy@users.noreply.github.com>
2022-03-10 22:26:32 +0530

    ok

--- Polyfills/Array/every.js
+++ Polyfills/Array/every.js
@@ -1,1 +1,10 @@
 
+Array.prototype.every = function (fun,argun)   {
+         if(!(typeof fun == "function" || fun instanceof Function)){
+            throw TypeError;    
+          }
+        var boll = [];
+       for(let i =0; i < this.length; i++)
+         boll.push(fun.call(argun,this[i],i,this))
+        return boll.includes(false)?false:true;
+ }
