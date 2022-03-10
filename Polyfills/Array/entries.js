5f7c4be08fd39a3ac6fbbc8d2ffffb3d963ff602
im__koli <51367985+koliboy@users.noreply.github.com>
2022-03-10 22:04:57 +0530

    ok

--- Polyfills/Array/entries.js
+++ Polyfills/Array/entries.js
@@ -1,1 +1,19 @@
+    
+ Array.prototype.entries = function ()   {
+     ENTRIES = [];
+       for(let i =0; i < this.length; i++)
+        ENTRIES.push([i,this[i]])
+        return ENTRIES[Symbol.iterator]();
+ }
 
+
+
+
+/*  !Symbol Use This  code */
+ Array.prototype.entries = function ()   {
+     ENTRIES = [];
+       for(let i =0; i < this.length; i++)
+        ENTRIES.push([i,this[i]])
+        return ENTRIES;
+ }
+/* for(enties of array) */
