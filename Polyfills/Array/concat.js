1440b06c7d0f31422384da926c433165f198f3b5
im__koli <51367985+koliboy@users.noreply.github.com>
2022-03-10 13:04:39 +0530

    ok

--- Polyfills/Array/concat.js
+++ Polyfills/Array/concat.js
@@ -1,1 +1,12 @@
-
+ Array.prototype.concat = function (){
+       
+        for(con_add of  Array.from(arguments)){
+            if(con_add instanceof Array){
+                for(c_2  of con_add)
+                 this.push(c_2)
+            }else this.push(con_add);
+             
+        }
+        
+         return this
+   }
