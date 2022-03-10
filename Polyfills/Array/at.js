f145aa5ba96a4e14450c85d1738dd681dc756f1b
im__koli <51367985+koliboy@users.noreply.github.com>
2022-03-10 13:23:28 +0530

    Update at.js

--- Polyfills/Array/at.js
+++ Polyfills/Array/at.js
@@ -1,1 +1,10 @@
-
+ Array.prototype.at = function (index){
+          if((isNaN(index)))
+          return 1;
+          else if( 0 < index) return this[index];
+         else {
+             var len = this.length;
+             var this_resver=   len  += index; 
+             return   this[this_resver]
+         }
+   }
