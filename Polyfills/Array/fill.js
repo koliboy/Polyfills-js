1696ca8e4f0715d4934916b35a09b4e60b7ba870
im__koli <51367985+koliboy@users.noreply.github.com>
2022-03-11 00:46:01 +0530

    ok

--- Polyfills/Array/fill.js
+++ Polyfills/Array/fill.js
@@ -1,16 +1,18 @@
-
-
-Array.prototype.fill = function ()  {
-     var [val,start,end] = [
-         arguments[1] ?? 0, arguments[2] ??this.length];
-         if(start < 0){
-             let r = this.length;
-             start = r+= start;
-         }else if (end < 0){
-             let r_2 = this.length;
-             end = r_2 +=end;
-         }
-         for(let i = start; i < end; i++)
-         this[i] =val;
-         return this;
- } 
+ Array.prototype.fill = function (){
+    var [val,start,end] = [arguments[0],
+       arguments[1] ?? 0, arguments[2] ?? this.length
+    ];
+    if(start < 0){
+        let r =this.length;
+         start = r+= start;
+        
+    }else if(end < 0){
+        let r_2 =this.length;
+         end = r_2+= end;
+        
+    }
+      for(let i = start; i < end; i++){
+              this[i] =val
+    }
+   return this;
+}
