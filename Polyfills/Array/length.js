295835c6f8c36f1797c8d83fb30403e331d1dcfd
im__koli <51367985+koliboy@users.noreply.github.com>
2022-03-10 12:45:17 +0530

    ok

--- Polyfills/Array/length.js
+++ Polyfills/Array/length.js
@@ -1,4 +1,4 @@
-Object.defineProperty(Array.prototype,"length",{
+ Object.defineProperty(Array.prototype,"length",{
        get(){
             var step = 0;
          for(count of this)
@@ -9,22 +9,27 @@ Object.defineProperty(Array.prototype,"length",{
           if(!isNaN(length)){
             var new_array = [];
             for(let  i = 0; i < length; i++){
-              
-             new_array.push(this[i]);           // old value
+               new_array.push(this[i]);           // old value
             }
-             var childThis = []
-          for(postion  of Object.keys( globalThis)){
+     var childThis = []
+    
+     for(postion  of Object.keys( globalThis)){
             if(globalThis[postion] == this){        // foo == foo LIke
-                 childThis.push(postion)}}           // LIke Other  global This global  other Example  a = foo; b ==  a;-> [foo,a,b] ;
-          globalThis[childThis[0]] = new_array;  // foo Postion Change = []
-          for(other_thisGlobals of childThis.slice(1)){
-              globalThis[other_thisGlobals] =   globalThis[childThis[0]];  // Again Global  postion set   after ( a = foo , b = foo )New Postion Global Value
-          }
-        }
+                 childThis.push(postion)
+                 // LIke Other  global This global  other Example  a = foo; b ==  a;-> [foo,a,b] ;
+            }
+         
+       }          
+          
+        globalThis[childThis[0]] = new_array;  // foo Postion Change = []
+     for(other_thisGlobals of childThis.slice(1)){
+      globalThis[other_thisGlobals] =   globalThis[childThis[0]];  // Again Global  postion set   after ( a = foo , b = foo )New Postion Global Value
+     }
+        
+              
+     }
        
         
-      }
+}
      
  }) ; 
-
-
