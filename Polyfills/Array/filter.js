a156692875cd2e122f4d779afeca8e3986c2d7d3
im__koli <51367985+koliboy@users.noreply.github.com>
2022-03-11 01:14:54 +0530

    ok

--- Polyfills/Array/filter.js
+++ Polyfills/Array/filter.js
@@ -1,1 +1,12 @@
-
+Array.prototype.filter = function (fun,arg){
+     if(!(typeof fun == "function" || fun instanceof Function)){
+            throw TypeError;    
+          }
+         FILTER = [];
+       for(let i =0; i < this.length; i++){
+        if(fun.call(arg,this[i],i,this)){
+             FILTER.push(this[i])
+        } 
+       }
+        return FILTER;
+}
