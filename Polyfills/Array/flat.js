f551a7ec8706c516a1707592d37eac6cdb1d9da5
im__koli <51367985+koliboy@users.noreply.github.com>
2022-03-12 00:04:48 +0530

    ok

--- Polyfills/Array/flat.js
+++ Polyfills/Array/flat.js
@@ -1,20 +1,20 @@
-function flat (arry)  {
-   
-  for( let  i = 0;
-       i < 1; i++
-      
-){
-  for(c of arry){
-     if(c instanceof Array){
-        if(count > 0) { document.console = count > 0
-            count--
-            flat(c)
-        }else{
-          b.push(c)  
-        }  
-     }else {
-        b.push(c)
-     }
-  }
+var b= [0,1,[1,2,3,3],[1,2,3,4,4,5]
+    ];
+ for(let i = 0; i !=1;i++){
+      cutter()
  }
-}  
+   
+function cutter(){
+   let  New_JOb = [];
+     for(a of b){
+       if(a instanceof Array){
+           a.forEach(function(el){
+               New_JOb.push(el)
+           })
+       }else {
+         New_JOb.push(a)  
+       }
+     }
+     b = New_JOb;
+}
+ 
