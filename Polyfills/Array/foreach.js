90a28243c6e26e6f430ae0dc87fc58157d5eec04
im__koli <51367985+koliboy@users.noreply.github.com>
2022-03-12 01:48:26 +0530

    ok

--- Polyfills/Array/foreach.js
+++ Polyfills/Array/foreach.js
@@ -1,1 +1,8 @@
-
+Array.prototype.forEach = function(fun,arg){
+    if(!(typeof fun == "function" || fun instanceof Function)){
+            throw TypeError;    
+          }
+   for(let i =0; i  < this.length; i++){
+       fun.call(arg,this[i],i,this);
+   }
+}
