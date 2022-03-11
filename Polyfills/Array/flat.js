2ba0444da32ffdafe73960b259f6e4f8313493dd
im__koli <51367985+koliboy@users.noreply.github.com>
2022-03-11 20:59:36 +0530

    ok

--- Polyfills/Array/flat.js
+++ Polyfills/Array/flat.js
@@ -1,1 +1,20 @@
-
+function flat (arry)  {
+   
+  for( let  i = 0;
+       i < 1; i++
+      
+){
+  for(c of arry){
+     if(c instanceof Array){
+        if(count > 0) { document.console = count > 0
+            count--
+            flat(c)
+        }else{
+          b.push(c)  
+        }  
+     }else {
+        b.push(c)
+     }
+  }
+ }
+}  
