580d69a6a67fab78c7c86b22345e6e2fde9bf5e1
im__koli <51367985+koliboy@users.noreply.github.com>
2022-03-12 02:07:11 +0530

    ok

--- Polyfills/Array/from.js
+++ Polyfills/Array/from.js
@@ -1,1 +1,13 @@
-
+Array.from = function(array,fun,arg){
+      var new_JOB = [];
+         if(array[Symbol.iterator]){
+             for(let i = 0 ; i < array.length;i++){
+                 if(fun instanceof Function){
+                    new_JOB.push(fun.call(arg,array[i],i,array))
+                 } else {
+                     new_JOB.push(array[i])
+                 }
+             }
+         }
+         return new_JOB;
+}
