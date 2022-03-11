963e5a459a3d656048170ff95700ecd9cd613e35
im__koli <51367985+koliboy@users.noreply.github.com>
2022-03-11 16:04:44 +0530

    ok

--- Polyfills/Array/findIndex.js
+++ Polyfills/Array/findIndex.js
@@ -1,1 +1,14 @@
+ Array.prototype.findIndex = function (fun,arg){
+     if(!(typeof fun == "function" || fun instanceof Function)){
+            throw TypeError;    
+          }
+      
+       for(let i =0; i < this.length; i++){
+        if(fun.call(arg,this[i],i,this)){
+            return i
+        } 
+       }
+        
+}
+
 
