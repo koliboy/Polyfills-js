0e888d16129beef54323857b9a7a959ffd0ba79c
im__koli <51367985+koliboy@users.noreply.github.com>
2022-03-12 01:24:32 +0530

    ok

--- Polyfills/Array/flatMap.js
+++ Polyfills/Array/flatMap.js
@@ -1,1 +1,10 @@
-
+Array.prototype.flatMap = function(fun,arg){
+    if(!(typeof fun == "function" || fun instanceof Function)){
+            throw TypeError;    
+          }
+    var flat_M = this,new_JOb = []; 
+     for(let i =0; i  < flat_M.length;i++){
+         new_JOb.push(fun.call(arg,this[i],i,this));
+         }      
+     return new_JOb.flat();
+}
