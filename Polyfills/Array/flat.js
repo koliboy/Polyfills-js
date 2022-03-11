0a4181b158bbbedf200d4af2b9796d6663b5d130
im__koli <51367985+koliboy@users.noreply.github.com>
2022-03-12 00:30:39 +0530

    ok

--- Polyfills/Array/flat.js
+++ Polyfills/Array/flat.js
@@ -1,20 +1,28 @@
-var b= [0,1,[1,2,3,3],[1,2,3,4,4,5]
-    ];
- for(let i = 0; i !=1;i++){
-      cutter()
- }
-   
+Array.prototype.flat = function(depth){
+    var b = this, depth_ = depth ?? 1;      
+        
+let i = 0;
+do{
+  i++
+  cutter();
+}while( i !=depth_);
+
 function cutter(){
    let  New_JOb = [];
-     for(a of b){
+for(
+     a of b
+){
        if(a instanceof Array){
            a.forEach(function(el){
                New_JOb.push(el)
-           })
-       }else {
+           });
+       }else if(!( typeof  a == 'undefined')) {
          New_JOb.push(a)  
        }
      }
      b = New_JOb;
+}    
+ return b       
 }
+ 
  
